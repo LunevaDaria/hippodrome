75a3c5081fe58ae09146f3abe9cf9bee05546e8c
Daria Luneva <luneva.daria@mail.ru>
2023-06-01 12:56:53 +0300

    Final project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+    private static final Logger log = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) throws Exception {
+
         List<Horse> horses = List.of(
                 new Horse("Буцефал", 2.4),
                 new Horse("Туз Пик", 2.5),
@@ -15,6 +21,8 @@ public class Main {
         );
         Hippodrome hippodrome = new Hippodrome(horses);
 
+        log.info("Начало скачек. Количество участников: " + horses.size());
+
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
             watch(hippodrome);
@@ -23,6 +31,8 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println("Победил " + winnerName + "!");
+
+        log.info("Окончание скачек. Победитель: " + winnerName);
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
